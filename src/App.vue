f34b6c5e4e4723a08674ea0423838d0cd41c6854
AdamRB208 <adamrborup@gmail.com>
2025-04-10 15:46:42 -0600

    Init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { computed } from 'vue'
+import { AppState } from './AppState.js'
+
+
+
+
+
+</script>
+
+<template>
+  <header>
+    <div class="container d-flex justify-content-center">
+      <div class="row">
+        <div class="col-12 mb-3 mt-3">
+          <h1>🌘MoonMiner🌒</h1>
+        </div>
+      </div>
+    </div>
+  </header>
+  <main>
+
+  </main>
+  <footer class=" text-center">
+    Made with <i class="mdi mdi-vuejs  text-success"></i> by @AdamRB208
+  </footer>
+</template>
+
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+header {
+  background-color: gray;
+}
+</style>
